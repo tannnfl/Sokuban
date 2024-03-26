5f1ecdc5908c394dc4061a1ea29df3aa840d4ca1
Fanglin Tan <ft2159@nyu.edu>
2024-03-25 22:14:47 -0400

    Event Declare for Player

--- Assets/Sokoban Starter/Scripts/Player.cs
+++ Assets/Sokoban Starter/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class Player : MonoBehaviour
     public KeyCode Left = KeyCode.A;
     public KeyCode Right = KeyCode.D;
 
+    public UnityVector2Event MoveUp;
+    public UnityVector2Event MoveDown;
+    public UnityVector2Event MoveLeft;
+    public UnityVector2Event MoveRight;
 
 
     // Start is called before the first frame update
@@ -24,19 +29,26 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(Up))
         {
-
+            MoveUp.Invoke(GetComponent<GridObject>().gridPosition);
         }
         else if (Input.GetKeyDown(Down))
         {
-
+            MoveDown.Invoke(GetComponent<GridObject>().gridPosition);
         }
         else if (Input.GetKeyDown(Left))
         {
-
+            MoveLeft.Invoke(GetComponent<GridObject>().gridPosition);
         }
         else if (Input.GetKeyDown(Right))
         {
-
+            MoveRight.Invoke(GetComponent<GridObject>().gridPosition);
         }
-    }
+
+     }    
+}
+
+    [System.Serializable]
+    public class UnityVector2Event : UnityEvent<Vector2>
+{
+
 }
