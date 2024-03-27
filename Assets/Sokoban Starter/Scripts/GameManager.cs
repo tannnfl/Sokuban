a3ade3b8843a00742623fa23f753da1938e6f80e
Fanglin Tan <ft2159@nyu.edu>
2024-03-27 02:24:46 -0400

    final push

--- Assets/Sokoban Starter/Scripts/GameManager.cs
+++ Assets/Sokoban Starter/Scripts/GameManager.cs
@@ -8,45 +8,170 @@ namespace block
     {
         public static void Attempt(Vector2Int movement, GameObject obj)
         {
-            //grid position of attempting grid
-            int _x = (obj.GetComponent<GridObject>().gridPosition + movement).x;
-            int _y = (obj.GetComponent<GridObject>().gridPosition + movement).y;
-            //check not out of boundary
-            if (0 < _x && _x <= 10 &&
-                0 < _y && _y <= 5)
+            if (obj.tag == "player" || obj.tag == "smooth" || obj.tag == "sticky")
             {
-                //check if next grid is empty
-                if (FindAttemptingBlock(movement, obj) == null)
+                //grid position of attempting grid
+                int _x = (obj.GetComponent<GridObject>().gridPosition + movement).x;
+                int _y = (obj.GetComponent<GridObject>().gridPosition + movement).y;
+                //check not out of boundary
+                if (0 < _x && _x <= 10 &&
+                    0 < _y && _y <= 5)
                 {
-                    print("empty move");
-                    GameManager.Move(movement, obj);
-                }
-                else
-                {
-                    Attempt(movement, FindAttemptingBlock(movement, obj));
+                    //check if next grid is empty
                     if (FindAttemptingBlock(movement, obj) == null)
                     {
-                        print("blocking move");
                         Move(movement, obj);
+                        Pull(movement, obj);
+                    }
+                    else
+                    {
+                        Attempt(movement, FindAttemptingBlock(movement, obj));
+                        if (FindAttemptingBlock(movement, obj) == null)
+                        {
+                            Move(movement, obj);
+                            Pull(movement, obj);
+                        }
                     }
                 }
             }
         }
 
-        public static void Move(Vector2Int movement, GameObject obj)
+        //clingy box
+        public static void Pull(Vector2Int movement, GameObject obj)
         {
-            obj.GetComponent<GridObject>().gridPosition += movement;
-            print("move method");
+
+            //clingy 
+            if (FindAttemptingBlock(-movement * 2, obj) != null //check if pulled grid has block
+                && (FindAttemptingBlock(-movement * 2, obj).tag == "clingy" || FindAttemptingBlock(-movement * 2, obj).tag == "sticky")) //check if attempting to move obj moved
+            {
+                GameObject _obj = FindAttemptingBlock(-movement * 2, obj);
+                //grid position of attempting grid
+                int _x = (_obj.GetComponent<GridObject>().gridPosition + movement).x;
+                int _y = (_obj.GetComponent<GridObject>().gridPosition + movement).y;
+                //check not out of boundary
+                if (0 < _x && _x <= 10 &&
+                    0 < _y && _y <= 5)
+                {
+                    //check if next grid is empty
+                    if (FindAttemptingBlock(movement, _obj) == null)
+                    {
+                        _obj.GetComponent<GridObject>().gridPosition += movement;
+                        Pull(movement, _obj);
+                    }
+                    else
+                    {
+                        Attempt(movement, FindAttemptingBlock(movement, _obj));
+                        if (FindAttemptingBlock(movement, _obj) == null)
+                        {
+                            _obj.GetComponent<GridObject>().gridPosition += movement;
+                            Pull(movement, _obj);
+                        }
+                    }
+                }
+                //Attempt(movement, FindAttemptingBlock(-movement * 2, obj));
+            }
+
+            
+            //sticky left & right
+            Vector2Int direction = new Vector2Int (movement.y,movement.x);
+            if (FindAttemptingBlock(direction - movement,obj) != null
+                && FindAttemptingBlock(direction - movement, obj).tag == "sticky")
+            {
+                GameObject _obj = FindAttemptingBlock(direction - movement, obj);
+                //grid position of attempting grid
+                int _x = (_obj.GetComponent<GridObject>().gridPosition + movement).x;
+                int _y = (_obj.GetComponent<GridObject>().gridPosition + movement).y;
+                //check not out of boundary
+                if (0 < _x && _x <= 10 &&
+                    0 < _y && _y <= 5)
+                {
+                    //check if next grid is empty
+                    if (FindAttemptingBlock(movement, _obj) == null)
+                    {
+                        _obj.GetComponent<GridObject>().gridPosition += movement;
+                        Pull(movement, _obj);
+                    }
+                    else
+                    {
+                        Attempt(movement, FindAttemptingBlock(movement, _obj));
+                        if (FindAttemptingBlock(movement, _obj) == null)
+                        {
+                            _obj.GetComponent<GridObject>().gridPosition += movement;
+                            Pull(movement, _obj);
+                        }
+                    }
+                }
+                //Attempt(movement, FindAttemptingBlock(movement, obj));
+            }
+            if (FindAttemptingBlock(-direction - movement, obj) != null
+                && FindAttemptingBlock(-direction - movement, obj).tag == "sticky")
+            {
+                GameObject _obj = FindAttemptingBlock(-direction - movement, obj);
+                //grid position of attempting grid
+                int _x = (_obj.GetComponent<GridObject>().gridPosition + movement).x;
+                int _y = (_obj.GetComponent<GridObject>().gridPosition + movement).y;
+                //check not out of boundary
+                if (0 < _x && _x <= 10 &&
+                    0 < _y && _y <= 5)
+                {
+                    //check if next grid is empty
+                    if (FindAttemptingBlock(movement, _obj) == null)
+                    {
+                        _obj.GetComponent<GridObject>().gridPosition += movement;
+                        Pull(movement, _obj);
+                    }
+                    else
+                    {
+                        Attempt(movement, FindAttemptingBlock(movement, _obj));
+                        if (FindAttemptingBlock(movement, _obj) == null)
+                        {
+                            _obj.GetComponent<GridObject>().gridPosition += movement;
+                            Pull(movement, _obj);
+                        }
+                    }
+                }
+                //Attempt(movement, FindAttemptingBlock(-movement, obj));
+            }
+            
         }
 
-        public static GameObject FindAttemptingBlock(Vector2 movement, GameObject obj)
+        public static void Move(Vector2Int movement, GameObject obj)
         {
-            GameObject[] blockObjects = GameObject.FindGameObjectsWithTag("block");
-            foreach (GameObject blockObject in blockObjects)
+            switch (obj.tag)
             {
-                if ( obj.GetComponent<GridObject>().gridPosition + movement == blockObject.GetComponent<GridObject>().gridPosition)
+                case "player":
+                    //keyboard command to move; push other
+                    obj.GetComponent<GridObject>().gridPosition += movement;
+                    print("player move");
+                    break;
+                case "smooth":
+                    //can be pushed to move; push other
+                    obj.GetComponent<GridObject>().gridPosition += movement;
+                    print("smooth move");
+                    break;
+                case "wall":
+                    //cannot be pushed to move or push other
+                    print("wall stay");
+                    break;
+                case "clingy":
+                    //inverse of smooth, can only be pulled; can't be pushed
+                    print("clingy stay");
+                    break;
+                case "sticky":
+                    //stick to moving block; if attempting grid is blocked, stop
+                    obj.GetComponent<GridObject>().gridPosition += movement;
+                    break;
+            }
+        }
+
+        public static GameObject FindAttemptingBlock(Vector2Int movement, GameObject obj)
+        {
+            GridObject[] blockObjects = GameObject.FindObjectsOfType<GridObject>();
+            foreach(GridObject blockObject in blockObjects)
+            {
+                if ( obj.GetComponent<GridObject>().gridPosition + movement == blockObject.gridPosition)
                 {
-                    return blockObject;
+                    return blockObject.gameObject;
                 }
             }
             return null;
